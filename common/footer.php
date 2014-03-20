8fc8b13fd5fc68b2f4fb2f67d6c62928aad9c4b4
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-20 19:17:39 -0400

    Added navigation links
    Added navigation links to the footer.

--- common/footer.php
+++ common/footer.php
@@ -1,3 +1,18 @@
 <div class="footer">
+	<table class="footer_table">
+		<tr>
+			<td class="footer_table_data_left"></td>
+			<td class="footer_table_data_links">
+				<p class="footer_links">resumé</p>
+			</td>
+			<td class="footer_table_data_links">
+				<p class="footer_links">contact me</p>
+			</td>
+			<td class="footer_table_data_links">
+				<p class="footer_links">credits</p>
+			</td>
+			<td class="footer_table_data_right"></td>
+		</tr>
+	</table>
 	<p class="footer_text">&copy; <script>document.write(new Date().getFullYear());</script> Viraj Chitnis</p>
 </div>

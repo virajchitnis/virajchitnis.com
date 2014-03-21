b736601cc3f2f8f91cb8c6e0c5d2958731011ac1
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-21 19:45:44 -0400

    Completed admin page

--- admin/index.php
+++ admin/index.php
@@ -2,32 +2,38 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="author" content="Viraj Chitnis">
+		<link rel="stylesheet" href="../css/admin.css">
 		<?php include("../common/headers.php"); ?>
 		<script src="../js/openlink.js"></script>
-		<title>Admin | Viraj Chitnis</title>
+		<title>Admin - Viraj Chitnis</title>
 	</head>
 	<body>
 		<div class="wrapper">
 			<div class="body">
 				<?php include("../common/header.php"); ?>
-				<p><a href="../">Home</a></p>
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-				<?php
-        			if (file_exists("../.git")) {
-        				$git_branch = exec("git branch | grep '*' | awk '{print $2}'");
-        				$branch;
-        				if ($git_branch == "master") {
-        					$branch = "stable";
-        				}
-        				else {
-        					$branch = $git_branch;
-        				}
-        		?>
-						<p><?php echo exec("git describe"); ?> (<?php echo $branch; ?>) <button onclick="openLink('../scripts/gitupdate.php')">Update</button></p>
-				<?php
-					}
-				?>
+				<div class="body_content">
+								<p class="body_table_content_text_space">&nbsp;</p>
+								<h2 class="body_table_content_text">Admin</h2>
+								<p class="body_table_content_text_space">&nbsp;</p>
+								<?php
+				        			if (file_exists("../.git")) {
+				        				$git_branch = exec("git branch | grep '*' | awk '{print $2}'");
+				        				$branch;
+				        				if ($git_branch == "master") {
+				        					$branch = "stable";
+				        				}
+				        				else {
+				        					$branch = $git_branch;
+				        				}
+				        		?>
+										<p class="body_table_content_text"><?php echo exec("git describe"); ?> (<?php echo $branch; ?>) <button onclick="openLink('../scripts/gitupdate.php')">Update</button></p>
+								<?php
+									}
+								?>
+								<p class="body_table_content_text_space">&nbsp;</p>
+								<p><a class="body_href" href="../">Exit</a></p>
+								<p class="body_table_content_text_space">&nbsp;</p>
+				</div>
 			</div>
 			<div class="push"></div>
 		</div>

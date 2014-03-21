5e478bf42d305d50fe8293c58bef10b54452b220
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-21 18:04:59 -0400

    Added credit page and gentoo logo

---
+++ credits/index.php
@@ -0,0 +1,49 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="author" content="Viraj Chitnis">
+		<link rel="stylesheet" href="../css/resume.css">
+		<?php include("../common/headers.php"); ?>
+		<title>Credits | Viraj Chitnis</title>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="body">
+				<?php include("../common/header.php"); ?>
+				<div class="body_content">
+								<p class="body_table_content_text_space">&nbsp;</p>
+								<h2 class="body_table_content_text">credits</h2>
+								<p class="body_table_content_text_space">&nbsp;</p>
+								<table class="body_table">
+										<td class="body_table_headers">
+											<h2 class="body_table_content_text">background</h2>
+										</td>
+										<td class="body_table_data">
+											<p class="body_table_content_text">The photograph being used as the background image for this website was taken by my brother Rajas Chitnis during our family holiday to Shenandoa National Park in May 2012.</p>
+										</td>
+									</tr>
+									<tr>
+										<td class="body_table_headers">
+											<h2 class="body_table_content_text">font</h2>
+										</td>
+										<td class="body_table_data">
+											<p class="body_table_content_text">The font used for this website is called Architect's Daughter and is designed by Kimberly Geswein. It is an open font and is available at the Open Font Library.</p>
+										</td>
+									</tr>
+									<tr>
+										<td class="body_table_headers">
+											<h2 class="body_table_content_text">server</h2>
+										</td>
+										<td class="body_table_data">
+											<p class="body_table_content_text"><a href="http://www.gentoo.org"><img src="../images/gentoo_badge.png"></a></p>
+										</td>
+									</tr>
+								</table>
+								<p class="body_table_content_text_space">&nbsp;</p>
+				</div>
+			</div>
+			<div class="push"></div>
+		</div>
+		<?php include("../common/footer.php"); ?>
+    </body>
+</html>

da007ff2c9e3ce0e16835eb6615257528cc9e432
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-20 23:54:10 -0400

    Dynamic background image size change now works

--- index.php
+++ index.php
@@ -2,16 +2,15 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="author" content="Viraj Chitnis">
-		<meta name="viewport" content="width=width">
 		<link rel="stylesheet" href="css/design.css">
 		<link rel="stylesheet" href="css/index.css">
-		<link rel="stylesheet" media="only screen and (max-width: 432px)" href="css/background-432x324.css">
-		<link rel="stylesheet" media="only screen and (max-width: 1080px)" href="css/background-1080x810.css">
-		<link rel="stylesheet" media="only screen and (max-width: 1296px)" href="css/background-1296x972.css">
-		<link rel="stylesheet" media="only screen and (max-width: 1728px)" href="css/background-1728x1296.css">
-		<link rel="stylesheet" media="only screen and (max-width: 2160px)" href="css/background-2160x1620.css">
-		<link rel="stylesheet" media="only screen and (max-width: 3240px)" href="css/background-3240x2430.css">
-		<link rel="stylesheet" media="only screen and (max-width: 4320px)" href="css/background-4320x3240.css">
+		<link rel="stylesheet" media="only screen and (max-width: 432px)" href="css/background-432-324.css">
+		<link rel="stylesheet" media="only screen and (min-width: 432px) and (max-width: 1080px)" href="css/background-1080-810.css">
+		<link rel="stylesheet" media="only screen and (min-width: 1080px) and (max-width: 1296px)" href="css/background-1296-972.css">
+		<link rel="stylesheet" media="only screen and (min-width: 1296px) and (max-width: 1728px)" href="css/background-1728-1296.css">
+		<link rel="stylesheet" media="only screen and (min-width: 1728px) and (max-width: 2160px)" href="css/background-2160-1620.css">
+		<link rel="stylesheet" media="only screen and (min-width: 2160px) and (max-width: 3240px)" href="css/background-3240-2430.css">
+		<link rel="stylesheet" media="only screen and (min-width: 3240px)" href="css/background-4320-3240.css">
 		<link rel="stylesheet" media="only screen" href="http://openfontlibrary.org/face/architect-s-daughter" rel="stylesheet" type="text/css"/>
 		<title>Viraj Chitnis</title>
 	</head>

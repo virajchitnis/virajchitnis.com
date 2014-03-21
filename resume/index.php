5e75b43c5226d019f5316d06a1508030129f9a0a
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-21 16:38:47 -0400

    Completed resumé layout

--- resume/index.php
+++ resume/index.php
@@ -12,16 +12,48 @@
 				<?php include("../common/header.php"); ?>
 				<div class="body_content">
 								<p class="body_table_content_text_space">&nbsp;</p>
-								<h2 class="body_table_content_text">Viraj Chitnis</h2>
-								<p class="body_table_content_text"><a href="mailto:chitnisviraj@gmail.com">chitnisviraj@gmail.com</a></p>
-								<p class="body_table_content_text">I am crazy about computers, gadgets, cars and any kind of technology. 
-								I love programming and have built multiple web applications.</p>
-								<p class="body_table_content_text">I am from Mumbai, India and I currently live in Wynnewood, PA</p>
-								<p class="body_table_content_text">I am majoring in Information Science & Technology at Temple University in Philadelphia, PA</p>
-								<p class="body_table_content_text">I program in PHP, HTML, CSS, JavaScript, Java, C, C#, Objective-C and SQL</p>
-								<p class="body_table_content_text">I am an expert in Linux server setup and management. I run multiple Linux servers at home 
-								which host web , database , mail, dhcp, dns, web proxy and storage services.</p>
-								<p class="body_table_content_text">I also troubleshoot and provide support to my family and friends for their computing needs.</p>
+								<table class="body_table">
+									<tr>
+										<td class="body_table_headers">
+											<h2 class="body_table_content_text">Viraj Chitnis</h2>
+										</td>
+										<td class="body_table_data"></td>
+									</tr>
+									<tr>
+										<td class="body_table_headers">
+											<h2 class="body_table_content_text">about me</h2>
+										</td>
+										<td class="body_table_data">
+											<p class="body_table_content_text">I am crazy about computers, gadgets, cars and any kind of technology. I love programming and have built multiple web applications.</p>
+										</td>
+									</tr>
+									<tr>
+										<td class="body_table_headers">
+											<h2 class="body_table_content_text">location</h2>
+										</td>
+										<td class="body_table_data">
+											<p class="body_table_content_text">I am from Mumbai, India and I currently live in Wynnewood, PA.</p>
+										</td>
+									</tr>
+									<tr>
+										<td class="body_table_headers">
+											<h2 class="body_table_content_text">education</h2>
+										</td>
+										<td class="body_table_data">
+											<p class="body_table_content_text">I am majoring in Information Science & Technology at Temple University.</p>
+										</td>
+									</tr>
+									<tr>
+										<td class="body_table_headers">
+											<h2 class="body_table_content_text">skills</h2>
+										</td>
+										<td class="body_table_data">
+											<p class="body_table_content_text">I program in PHP, HTML, CSS, JavaScript, Java, C, C#, Objective-C and SQL.</p>
+											<p class="body_table_content_text">I am an expert in Linux server setup and management. I run multiple Linux servers at home which host web , database , mail, dhcp, dns, web proxy and storage services.</p>
+											<p class="body_table_content_text">I also troubleshoot and provide support to my family and friends for their computing needs.</p>
+										</td>
+									</tr>
+								</table>
 								<p class="body_table_content_text_space">&nbsp;</p>
 				</div>
 			</div>

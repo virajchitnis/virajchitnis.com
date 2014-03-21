5db39541a005dfcc04ce0a2598f214934331c62a
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-21 17:28:16 -0400

    Added more info to my resume

--- resume/index.php
+++ resume/index.php
@@ -12,19 +12,14 @@
 				<?php include("../common/header.php"); ?>
 				<div class="body_content">
 								<p class="body_table_content_text_space">&nbsp;</p>
+								<h2 class="body_table_content_text">Viraj Chitnis</h2>
+								<p class="body_table_content_text_space">&nbsp;</p>
 								<table class="body_table">
-									<tr>
-										<td class="body_table_headers">
-											<h2 class="body_table_content_text">Viraj Chitnis</h2>
-										</td>
-										<td class="body_table_data"></td>
-									</tr>
-									<tr>
 										<td class="body_table_headers">
 											<h2 class="body_table_content_text">about me</h2>
 										</td>
 										<td class="body_table_data">
-											<p class="body_table_content_text">I am crazy about computers, gadgets, cars and any kind of technology. I love programming and have built multiple web applications.</p>
+											<p class="body_table_content_text">I am crazy about computers, gadgets, cars and any kind of technology. I love programming and building amazing things. I also love to travel and have visited 17 countries.</p>
 										</td>
 									</tr>
 									<tr>
@@ -32,7 +27,7 @@
 											<h2 class="body_table_content_text">location</h2>
 										</td>
 										<td class="body_table_data">
-											<p class="body_table_content_text">I am from Mumbai, India and I currently live in Wynnewood, PA.</p>
+											<p class="body_table_content_text">From Mumbai, India, currently living in Wynnewood, PA.</p>
 										</td>
 									</tr>
 									<tr>
@@ -40,7 +35,7 @@
 											<h2 class="body_table_content_text">education</h2>
 										</td>
 										<td class="body_table_data">
-											<p class="body_table_content_text">I am majoring in Information Science & Technology at Temple University.</p>
+											<p class="body_table_content_text">Majoring in Information Science & Technology at Temple University.</p>
 										</td>
 									</tr>
 									<tr>
@@ -48,9 +43,19 @@
 											<h2 class="body_table_content_text">skills</h2>
 										</td>
 										<td class="body_table_data">
-											<p class="body_table_content_text">I program in PHP, HTML, CSS, JavaScript, Java, C, C#, Objective-C and SQL.</p>
-											<p class="body_table_content_text">I am an expert in Linux server setup and management. I run multiple Linux servers at home which host web , database , mail, dhcp, dns, web proxy and storage services.</p>
-											<p class="body_table_content_text">I also troubleshoot and provide support to my family and friends for their computing needs.</p>
+											<p class="body_table_content_text">Program in PHP, HTML, CSS, JavaScript, Java, C, C#, Objective-C and SQL.</p>
+											<p class="body_table_content_text">Expert in Linux server setup and management and run multiple Linux servers at home which host web, database, mail, dhcp, dns, web proxy and storage services.</p>
+											<p class="body_table_content_text">Troubleshoot and provide support to my family and friends for their computing needs.</p>
+											<p class="body_table_content_text">This website serves as an example of my work and is hosted on one of my servers at home. If you wish to see more of my work, get in touch with me via email and I will send you the links to my other work.</p>
+										</td>
+									</tr>
+									<tr>
+										<td class="body_table_headers">
+											<h2 class="body_table_content_text">experience</h2>
+										</td>
+										<td class="body_table_data">
+											<p class="body_table_content_text">Interned at Career Knowledge Resources in Mumbai, India during summer 2009.</p>
+											<p class="body_table_content_text">Interned at Bisil in Philadelphia, PA during summer 2013.</p>
 										</td>
 									</tr>
 								</table>

e22cd87868dc66c68633c1c4df67b682da6db29f
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-20 20:08:21 -0400

    Removed table from resumé

--- resume/index.php
+++ resume/index.php
@@ -13,10 +13,6 @@
 			<div class="body">
 				<?php include("../common/header.php"); ?>
 				<div class="body_content">
-					<table class="body_table">
-						<td class="body_table_data_left"></td>
-						<td class="body_table_data_center">
-							<div class="body_table_content">
 								<p class="body_table_content_text_space">&nbsp;</p>
 								<h2 class="body_table_content_text">Viraj Chitnis</h2>
 								<p class="body_table_content_text"><a href="mailto:chitnisviraj@gmail.com">chitnisviraj@gmail.com</a></p>
@@ -29,10 +25,6 @@
 								which host web , database , mail, dhcp, dns, web proxy and storage services.</p>
 								<p class="body_table_content_text">I also troubleshoot and provide support to my family and friends for their computing needs.</p>
 								<p class="body_table_content_text_space">&nbsp;</p>
-							</div>
-						</td>
-						<td class="body_table_data_right"></td>
-					</table>
 				</div>
 			</div>
 			<div class="push"></div>

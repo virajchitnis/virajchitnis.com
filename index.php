9e1e296424c20de96ef88981d90814e392476818
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-20 23:33:28 -0400

    Added css for dynamic screen sizes and backgrounds

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="author" content="Viraj Chitnis">
+		<meta name="viewport" content="width=width">
 		<link rel="stylesheet" href="css/design.css">
 		<link rel="stylesheet" href="css/index.css">
-		<link rel="stylesheet" href="css/background-image.css">
-		<link rel="stylesheet" media="screen" href="http://openfontlibrary.org/face/architect-s-daughter" rel="stylesheet" type="text/css"/>
+		<link rel="stylesheet" media="only screen and (max-width: 432px)" href="css/background-432x324.css">
+		<link rel="stylesheet" media="only screen and (max-width: 1080px)" href="css/background-1080x810.css">
+		<link rel="stylesheet" media="only screen and (max-width: 1296px)" href="css/background-1296x972.css">
+		<link rel="stylesheet" media="only screen and (max-width: 1728px)" href="css/background-1728x1296.css">
+		<link rel="stylesheet" media="only screen and (max-width: 2160px)" href="css/background-2160x1620.css">
+		<link rel="stylesheet" media="only screen and (max-width: 3240px)" href="css/background-3240x2430.css">
+		<link rel="stylesheet" media="only screen and (max-width: 4320px)" href="css/background-4320x3240.css">
+		<link rel="stylesheet" media="only screen" href="http://openfontlibrary.org/face/architect-s-daughter" rel="stylesheet" type="text/css"/>
 		<title>Viraj Chitnis</title>
 	</head>
 	<body>

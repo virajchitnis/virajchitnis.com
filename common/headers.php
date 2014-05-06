4a8d139101329ba6939cc0c9d7685fadd35157da
Viraj Chitnis <chitnisviraj@gmail.com>
2014-05-06 11:14:02 -0400

    Changed Google Analytics script addition
    Detect if the Google Analytics script file exists and add the script if it exists.

--- common/headers.php
+++ common/headers.php
@@ -30,19 +30,7 @@
 		<link rel="stylesheet" media="only screen" href="../css/font-architects-daughter.css" rel="stylesheet" type="text/css"/>
 <?php
 	}
-	$git_branch = exec("git branch | grep '*' | awk '{print $2}'");
-	if ($git_branch == "master") {
-?>
-		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-		  ga('create', 'UA-49231243-1', 'virajchitnis.com');
-		  ga('send', 'pageview');
-
-		</script>
-<?php
+	if (file_exists("google_analytics.php")) {
+		include("google_analytics.php");
 	}
 ?>

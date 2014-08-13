3a92739844fa92b55548b8d6f112354c347ff04c
Viraj Chitnis <chitnisviraj@gmail.com>
2014-08-12 22:50:00 -0400

    Added appversions.php
    This file will return JSON that can be used by my apps to check for update availability.

---
+++ api/appversions.php
@@ -0,0 +1,23 @@
+<?php
+// Array of movie versions.
+$all_apps = array(
+	"Movies" => array(
+		0 => "v1.0",
+		1 => "v2.0",
+	),
+	"Weight" => array(
+		0 => "v1.0",
+		1 => "v1.0.1",
+		2 => "v1.0.2",
+		3 => "v1.1",
+		4 => "v1.1.1",
+		5 => "v1.1.2",
+	),
+	"TravelCountant" => array(
+		0 => "v1.0",
+		1 => "v1.1",
+	),
+);
+
+echo '<pre>'.json_encode($all_apps, JSON_PRETTY_PRINT).'</pre>';
+?>

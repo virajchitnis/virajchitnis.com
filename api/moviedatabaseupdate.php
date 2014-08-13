1095d4fbe238f4a0ab7f9707a5a16f1487c0eaab
Viraj Chitnis <chitnisviraj@gmail.com>
2014-08-13 16:25:27 -0400

    Added a response variable to the moviedatabaseupdate.php JSON

--- api/moviedatabaseupdate.php
+++ api/moviedatabaseupdate.php
@@ -3,6 +3,7 @@ $source = file_get_contents('http://www.virajchitnis.com/api/movies.php');
 
 $json = array(
 	"hash" => md5($source),
+	"response" => "True",
 );
 
 echo '<pre>'.json_encode($json, JSON_PRETTY_PRINT).'</pre>';

ff3ed5c3f1d9bbedaf839a7714c7c04b1690fda7
Viraj Chitnis <chitnisviraj@gmail.com>
2014-08-16 12:49:16 -0400

    Added TravelCountant to the apps page
    The apps page of the website now contains a section for TravelCountant.

--- apps/index.php
+++ apps/index.php
@@ -23,7 +23,15 @@
 											<h2 class="body_table_content_text">Weight & BMI Tracker</h2>
 										</td>
 										<td class="body_table_data">
-											<p class="body_table_content_text">The most challenging part of loosing and maintaining your weight is properly keeping track of it. This app will not only let you log your weight, but it will also help you by calculating your BMI and other weight management stats.<br><a href="https://appsto.re/us/e-bT1.i"><img src="../images/app_store_140px.png" width="140"></a></p>
+											<p class="body_table_content_text">The most challenging part of loosing and maintaining your weight is properly keeping track of it. This app will not only let you log your weight, but it will also help you by calculating your BMI and other weight management stats.<br><a href="http://itunes.apple.com/app/id900996996"><img src="../images/app_store_140px.png" width="140"></a></p>
+										</td>
+									</tr>
+									<tr>
+										<td class="body_table_headers">
+											<h2 class="body_table_content_text">TravelCountant - Simple Travel Expense Keeper</h2>
+										</td>
+										<td class="body_table_data">
+											<p class="body_table_content_text">If you want a simple solution for keeping track of your expenses during a vacation trip, or you want to keep track of your expenses for a project your working on, this is the app for you.<br>You can make multiple 'Pockets' in this app, a pocket is like an account, it defines the name of the trip/project, the amount that you have reserved for using with this endeavor, and the start date. You can then go about logging your expenses within each 'Pocket'. The expenses will be organized by day and at the bottom of each day you will be able to view the amount spent during that day and the amount remaining (balance carried forward).<br>Finally, once your trip is over, or your project is complete, you can generate an email with your expenses and send it to your accountant or to yourself to keep it in record.<br><a href="http://itunes.apple.com/app/id905751695"><img src="../images/app_store_140px.png" width="140"></a></p>
 										</td>
 									</tr>
 								</table>

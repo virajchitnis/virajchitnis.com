da84a1a7c183613a2fca12609da90bf67cfef28b
Viraj Chitnis <chitnisviraj@gmail.com>
2015-03-01 17:25:43 -0500

    Some more setup
    Signed-off-by: Viraj Chitnis <chitnisviraj@gmail.com>

--- app.js
+++ app.js
@@ -4,11 +4,31 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var configJSON = require('./config.json');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+// Disable x-powered-by header
+app.disable('x-powered-by');
+
+// node-sass setup
+var sassMiddleware = require('node-sass-middleware');
+app.use(sassMiddleware({
+	src: path.join(__dirname, 'src'),
+	dest: path.join(__dirname, 'public'),
+	debug: true,
+	outputStyle: 'compressed'
+}));
+app.use(express.static(path.join(__dirname, 'public/stylesheets')));
+
+// Morgan setup
+if (configJSON.deployment) {
+	app.enable('trust proxy');
+	var morgan = require('morgan');
+	app.use(morgan('combined'));
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,13 +43,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -37,23 +56,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 

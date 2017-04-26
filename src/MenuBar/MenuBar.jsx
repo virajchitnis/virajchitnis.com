3feb78df8b16f5f98d0c42fcbe84d8dff64ec099
Viraj Chitnis <chitnisviraj@gmail.com>
2017-04-26 17:54:06 -0400

    Added mobile navigation menu

--- src/MenuBar/MenuBar.jsx
+++ src/MenuBar/MenuBar.jsx
@@ -26,6 +26,23 @@ export default class MenuBar extends React.Component {
             <div className="nav-standard">credits</div>
           </div>
         </div>
+        <div className="container-mobile">
+          <div className="nav-link">
+            <div className="nav-standard">resumé</div>
+          </div>
+          <div className="nav-link">
+            <div className="nav-standard">blog</div>
+          </div>
+          <div className="nav-link">
+            <div className="nav-standard">winery</div>
+          </div>
+          <div className="nav-link">
+            <div className="nav-standard">credits</div>
+          </div>
+          <div className="nav-link">
+            <div className="nav-home">VC</div>
+          </div>
+        </div>
       </div>
     );
   }

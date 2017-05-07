5fd00088897c740a8c5079c57667367baa37f23f
Viraj Chitnis <chitnisviraj@gmail.com>
2017-05-07 19:50:15 -0400

    Fixed health and githook endpoints

--- server.js
+++ server.js
@@ -5,18 +5,10 @@ const app = express();
 const sys = require('sys')
 const exec = require('child_process').exec;
 const morgan = require('morgan');
+const crypto = require('crypto');
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'));
-
-app.use(function (req, res, next) {
-  res.status(404).sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
-
-app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
 
 app.get('/', function (req, res){
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
@@ -38,7 +30,8 @@ app.get('/credits', function (req, res){
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-app.get('/githook', function (req, res) {
+app.post('/githook', function (req, res) {
+  console.log('githook');
   xHubSig = req.headers['x-hub-signature'].substring(5);
   hmac = crypto.createHmac('sha1', 'somesecret');
   computedHubSig = hmac.digest('hex');
@@ -50,7 +43,17 @@ app.get('/githook', function (req, res) {
 });
 
 app.get('/health', function (req, res){
+  console.log('health');
     res.send(200, 'Up!');
+});
+
+app.use(function (req, res, next) {
+  res.status(404).sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port);

02cbe0fbdcafac0bfe4fddccdccc38f5619727ea
Viraj Chitnis <chitnisviraj@gmail.com>
2017-05-08 11:52:33 -0400

    Added Google Analytics

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -6,9 +6,17 @@ import WineryPage from '../WineryPage/WineryPage.jsx';
 import CreditsPage from '../CreditsPage/CreditsPage.jsx';
 import ErrorPage from '../ErrorPage/ErrorPage.jsx';
 import './global.scss';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-49231243-1');
 
 export default class App extends React.Component {
+  fireTracking() {
+    ReactGA.pageview(window.location.pathname);
+  }
+
   render() {
+    this.fireTracking();
+
     var dom;
     if (window.location.pathname == '/') {
       dom = (

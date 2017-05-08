1b8e0884f369d65cfb8d3a9139b1f34deffce139
Viraj Chitnis <chitnisviraj@gmail.com>
2017-05-07 21:24:26 -0400

    Fixing githook again

--- githook.sh
+++ githook.sh
@@ -2,7 +2,11 @@
 
 export PATH="/root/.nvm/versions/node/v6.10.1/bin:${PATH}"
 
+echo "Updating code..." >> ./logs/console.log
 git pull
 yarn
 yarn build
-forever restartall
+echo "Restarting..." >> ./logs/console.log
+./node_modules/forever/bin/forever stopall
+./node_modules/forever/bin/forever start -o ./logs/console.log server.js
+echo "Done updating." >> ./logs/console.log

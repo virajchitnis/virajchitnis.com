ca46da6c17e2f0e43862bf3754e1dcb2b1d35be3
Viraj Chitnis <chitnisviraj@gmail.com>
2017-05-07 22:02:52 -0400

    Revert "Fixing githook again"
    This reverts commit 1b8e0884f369d65cfb8d3a9139b1f34deffce139.

--- githook.sh
+++ githook.sh
@@ -2,11 +2,7 @@
 
 export PATH="/root/.nvm/versions/node/v6.10.1/bin:${PATH}"
 
-echo "Updating code..." >> ./logs/console.log
 git pull
 yarn
 yarn build
-echo "Restarting..." >> ./logs/console.log
-./node_modules/forever/bin/forever stopall
-./node_modules/forever/bin/forever start -o ./logs/console.log server.js
-echo "Done updating." >> ./logs/console.log
+forever restartall

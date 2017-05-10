41f84775e0082bb883492825392b8785253e79f0
Viraj Chitnis <chitnisviraj@gmail.com>
2017-05-09 23:39:20 -0400

    Added social icons

--- src/HomePage/MyInfo/MyInfo.jsx
+++ src/HomePage/MyInfo/MyInfo.jsx
@@ -9,9 +9,21 @@ export default class MyInfo extends React.Component {
   render() {
     return (
       <div className="my-info">
-        <img />
+        <img className="profile" />
         <h1>Viraj Chitnis</h1>
         <h2>web developer, ios developer, and hobbyist vintner</h2>
+        <div>
+          <a href="https://github.com/virajchitnis" target="_blank"><img className="github" /></a>
+        </div>
+        <div>
+          <a href="https://twitter.com/chitnisviraj" target="_blank"><img className="twitter" /></a>
+        </div>
+        <div>
+          <a href="https://www.instagram.com/virajchitnis/" target="_blank"><img className="instagram" /></a>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/viraj-chitnis-ba605894/" target="_blank"><img className="linkedin" /></a>
+        </div>
       </div>
     );
   }

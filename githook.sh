c47bfe2b8ad8ae557ed28fa319cf7300748e6171
Viraj Chitnis <chitnisviraj@gmail.com>
2017-05-27 15:15:26 -0400

    Fix deployment
    Fixes #15

--- githook.sh
+++ githook.sh
@@ -2,6 +2,7 @@
 
 export PATH="/root/.nvm/versions/node/v6.10.1/bin:${PATH}"
 
-git pull
+git fetch origin
+git checkout --force origin/master
 yarn redeploy
 ./node_modules/forever/bin/forever restartall

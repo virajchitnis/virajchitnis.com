c5f297c42033ed278afdf7e1c98cbac93b00e8ba
Viraj Chitnis <chitnisviraj@gmail.com>
2017-05-28 20:08:00 -0400

    Call API for displaying git commit ID in footer

--- src/common/components/Footer/Footer.jsx
+++ src/common/components/Footer/Footer.jsx
@@ -1,15 +1,66 @@
 import React from 'react';
+import request from 'superagent';
+import {getCookie} from '../../functions/Cookies.js';
 import './Footer.scss';
 
 export default class Footer extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      commit_id: '',
+      key: ''
+    };
+  }
+
+  componentDidMount() {
+    const key = getCookie('key');
+    this.setState({
+      key: key
+    });
+    if (key != '') {
+      this.getGitCommit(key);
+    }
+  }
+
+  getGitCommit(key) {
+    const self = this;
+    return request
+    .get('/api/gitcommit')
+    .set('api-key', key)
+    .end(function(err, res){
+      if (res.statusCode == 200) {
+        self.setState({
+          commit_id: res.body.commit_id
+        });
+      }
+    });
+  }
+
+  buildCommitElement() {
+    const commit_id = this.state.commit_id;
+    const key = this.state.key;
+    let commitFooter = '';
+    let commitURL = '';
+
+    if (commit_id != '') {
+      commitURL = "https://github.com/virajchitnis/virajchitnis.com/commit/" + commit_id;
+    }
+
+    if (key != '') {
+      commitFooter = (
+        <p className="commit">
+          <a href={commitURL}>{commit_id}</a>
+        </p>
+      );
+    }
+    return commitFooter;
   }
 
   render() {
     return (
       <div className="footer">
-        Copyright © 2017 Viraj Chitnis
+        <p className="copyright">Copyright © 2017 Viraj Chitnis</p>
+        {this.buildCommitElement()}
       </div>
     );
   }

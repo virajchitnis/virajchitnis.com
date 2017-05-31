02ccb0e2e999dbb8a79f3a3359ca8a6e1c224899
Viraj Chitnis <chitnisviraj@gmail.com>
2017-05-30 22:19:30 -0400

    Routing via events
    Closes #21

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -9,37 +9,143 @@ import KeyModal from '../common/components/KeyModal/KeyModal.jsx';
 import './global.scss';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: ''
+    };
+
+    this.resumeClick = this.resumeClick.bind(this);
+    this.appsClick = this.appsClick.bind(this);
+    this.homeClick = this.homeClick.bind(this);
+    this.wineryClick = this.wineryClick.bind(this);
+    this.creditsClick = this.creditsClick.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("resumeClick", this.resumeClick);
+    document.addEventListener("appsClick", this.appsClick);
+    document.addEventListener("homeClick", this.homeClick);
+    document.addEventListener("wineryClick", this.wineryClick);
+    document.addEventListener("creditsClick", this.creditsClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("resumeClick", this.resumeClick);
+    document.removeEventListener("appsClick", this.appsClick);
+    document.removeEventListener("homeClick", this.homeClick);
+    document.removeEventListener("wineryClick", this.wineryClick);
+    document.removeEventListener("creditsClick", this.creditsClick);
+  }
+
+  resumeClick() {
+    this.setState({
+      page: 'ResumePage'
+    });
+  }
+
+  appsClick() {
+    this.setState({
+      page: 'AppsPage'
+    });
+  }
+
+  homeClick() {
+    this.setState({
+      page: 'HomePage'
+    });
+  }
+
+  wineryClick() {
+    this.setState({
+      page: 'WineryPage'
+    });
+  }
+
+  creditsClick() {
+    this.setState({
+      page: 'CreditsPage'
+    });
+  }
+
   render() {
     var dom;
-    if (window.location.pathname == '/') {
-      dom = (
-        <HomePage />
-      );
-    }
-    else if (window.location.pathname == '/resume') {
-      dom = (
-        <ResumePage />
-      );
-    }
-    else if (window.location.pathname == '/apps') {
-      dom = (
-        <AppsPage />
-      );
-    }
-    else if (window.location.pathname == '/winery') {
-      dom = (
-        <WineryPage />
-      );
-    }
-    else if (window.location.pathname == '/credits') {
-      dom = (
-        <CreditsPage />
-      );
+    if (this.state.page === '') {
+      if (window.location.pathname == '/') {
+        dom = (
+          <HomePage />
+        );
+      }
+      else if (window.location.pathname == '/resume') {
+        dom = (
+          <ResumePage />
+        );
+      }
+      else if (window.location.pathname == '/apps') {
+        dom = (
+          <AppsPage />
+        );
+      }
+      else if (window.location.pathname == '/winery') {
+        dom = (
+          <WineryPage />
+        );
+      }
+      else if (window.location.pathname == '/credits') {
+        dom = (
+          <CreditsPage />
+        );
+      }
+      else {
+        dom = (
+          <ErrorPage />
+        );
+      }
     }
     else {
-      dom = (
-        <ErrorPage />
-      );
+      const page = this.state.page;
+      if (page === 'HomePage') {
+        window.history.pushState({}, "Viraj Chitnis", "/");
+        document.title = 'Viraj Chitnis';
+        dom = (
+          <HomePage />
+        );
+      }
+      else if (page === 'ResumePage') {
+        window.history.pushState({}, "Resumé - Viraj Chitnis", "/resume");
+        document.title = 'Resumé - Viraj Chitnis';
+        dom = (
+          <ResumePage />
+        );
+      }
+      else if (page === 'AppsPage') {
+        window.history.pushState({}, "Apps - Viraj Chitnis", "/apps");
+        document.title = 'Apps - Viraj Chitnis';
+        dom = (
+          <AppsPage />
+        );
+      }
+      else if (page === 'WineryPage') {
+        window.history.pushState({}, "Winery - Viraj Chitnis", "/winery");
+        document.title = 'Winery - Viraj Chitnis';
+        dom = (
+          <WineryPage />
+        );
+      }
+      else if (page === 'CreditsPage') {
+        window.history.pushState({}, "Credits - Viraj Chitnis", "/credits");
+        document.title = 'Credits - Viraj Chitnis';
+        dom = (
+          <CreditsPage />
+        );
+      }
+      else {
+        window.history.pushState({}, "Error - Viraj Chitnis", window.location.pathname);
+        document.title = 'Error - Viraj Chitnis';
+        dom = (
+          <ErrorPage />
+        );
+      }
     }
 
     return (
--- src/common/components/MenuBarContents/MenuBarContents.jsx
+++ src/common/components/MenuBarContents/MenuBarContents.jsx
@@ -6,44 +6,69 @@ export default class MenuBarContents extends React.Component {
     super(props);
   }
 
+  resumeClick() {
+    var resumeClickEvent = new Event('resumeClick');
+    document.dispatchEvent(resumeClickEvent);
+  }
+
+  appsClick() {
+    var appsClickEvent = new Event('appsClick');
+    document.dispatchEvent(appsClickEvent);
+  }
+
+  homeClick() {
+    var homeClickEvent = new Event('homeClick');
+    document.dispatchEvent(homeClickEvent);
+  }
+
+  wineryClick() {
+    var wineryClickEvent = new Event('wineryClick');
+    document.dispatchEvent(wineryClickEvent);
+  }
+
+  creditsClick() {
+    var creditsClickEvent = new Event('creditsClick');
+    document.dispatchEvent(creditsClickEvent);
+  }
+
   render() {
     return (
       <div>
         <div className="container">
           <div className="nav-link">
-            <a className="nav-standard" href='/resume'>resumé</a>
+            <a className="nav-standard" href="javascript:void(0);" onClick={this.resumeClick}>resumé</a>
           </div>
           <div className="nav-link">
-            <a className="nav-standard" href='/apps'>apps</a>
+            <a className="nav-standard" href="javascript:void(0);" onClick={this.appsClick}>apps</a>
           </div>
           <div className="nav-link">
-            <a className="nav-home" href='/'>VC</a>
+            <a className="nav-home" href="javascript:void(0);" onClick={this.homeClick}>VC</a>
           </div>
           <div className="nav-link">
-            <a className="nav-standard" href='/winery'>winery</a>
+            <a className="nav-standard" href="javascript:void(0);" onClick={this.wineryClick}>winery</a>
           </div>
           <div className="nav-link">
-            <a className="nav-standard" href='/credits'>credits</a>
+            <a className="nav-standard" href="javascript:void(0);" onClick={this.creditsClick}>credits</a>
           </div>
         </div>
         <div className="container-mobile">
           <div className="nav-link nav-link-top-home">
-            <a className="nav-home" href='/'>VC</a>
+            <a className="nav-home" href="javascript:void(0);" onClick={this.homeClick}>VC</a>
           </div>
           <div className="nav-link">
-            <a className="nav-standard" href='/resume'>resumé</a>
+            <a className="nav-standard" href="javascript:void(0);" onClick={this.resumeClick}>resumé</a>
           </div>
           <div className="nav-link">
-            <a className="nav-standard" href='/apps'>apps</a>
+            <a className="nav-standard" href="javascript:void(0);" onClick={this.appsClick}>apps</a>
           </div>
           <div className="nav-link">
-            <a className="nav-standard" href='/winery'>winery</a>
+            <a className="nav-standard" href="javascript:void(0);" onClick={this.wineryClick}>winery</a>
           </div>
           <div className="nav-link">
-            <a className="nav-standard" href='/credits'>credits</a>
+            <a className="nav-standard" href="javascript:void(0);" onClick={this.creditsClick}>credits</a>
           </div>
           <div className="nav-link nav-link-bottom-home">
-            <a className="nav-home" href='/'>VC</a>
+            <a className="nav-home" href="javascript:void(0);" onClick={this.homeClick}>VC</a>
           </div>
         </div>
       </div>

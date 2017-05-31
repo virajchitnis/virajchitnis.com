f0b416322354f4040e319d675d63a83b8e8fd2d9
Viraj Chitnis <chitnisviraj@gmail.com>
2017-05-31 08:32:51 -0400

    Fix back and forward buttons routing
    The website should route properly when the back or forward button is
    pressed.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -12,7 +12,8 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: ''
+      page: '',
+      backButtonClicked: false
     };
 
     this.resumeClick = this.resumeClick.bind(this);
@@ -20,6 +21,7 @@ export default class App extends React.Component {
     this.homeClick = this.homeClick.bind(this);
     this.wineryClick = this.wineryClick.bind(this);
     this.creditsClick = this.creditsClick.bind(this);
+    this.handlePopState = this.handlePopState.bind(this);
   }
 
   componentDidMount() {
@@ -28,6 +30,7 @@ export default class App extends React.Component {
     document.addEventListener("homeClick", this.homeClick);
     document.addEventListener("wineryClick", this.wineryClick);
     document.addEventListener("creditsClick", this.creditsClick);
+    window.addEventListener('popstate', this.handlePopState);
   }
 
   componentWillUnmount() {
@@ -36,6 +39,46 @@ export default class App extends React.Component {
     document.removeEventListener("homeClick", this.homeClick);
     document.removeEventListener("wineryClick", this.wineryClick);
     document.removeEventListener("creditsClick", this.creditsClick);
+    window.removeEventListener('popstate', this.handlePopState);
+  }
+
+  handlePopState(e) {
+    const pathname = window.location.pathname;
+    if (pathname === '/') {
+      this.setState({
+        page: 'HomePage',
+        backButtonClicked: true
+      });
+    }
+    else if (pathname === '/resume') {
+      this.setState({
+        page: 'ResumePage',
+        backButtonClicked: true
+      });
+    }
+    else if (pathname === '/apps') {
+      this.setState({
+        page: 'AppsPage',
+        backButtonClicked: true
+      });
+    }
+    else if (pathname === '/winery') {
+      this.setState({
+        page: 'WineryPage',
+        backButtonClicked: true
+      });
+    }
+    else if (pathname === '/credits') {
+      this.setState({
+        page: 'CreditsPage',
+        backButtonClicked: true
+      });
+    }
+    else {
+      this.setState({
+        backButtonClicked: true
+      });
+    }
   }
 
   resetScrollPosition() {
@@ -47,35 +90,40 @@ export default class App extends React.Component {
   resumeClick() {
     this.resetScrollPosition();
     this.setState({
-      page: 'ResumePage'
+      page: 'ResumePage',
+      backButtonClicked: false
     });
   }
 
   appsClick() {
     this.resetScrollPosition();
     this.setState({
-      page: 'AppsPage'
+      page: 'AppsPage',
+      backButtonClicked: false
     });
   }
 
   homeClick() {
     this.resetScrollPosition();
     this.setState({
-      page: 'HomePage'
+      page: 'HomePage',
+      backButtonClicked: false
     });
   }
 
   wineryClick() {
     this.resetScrollPosition();
     this.setState({
-      page: 'WineryPage'
+      page: 'WineryPage',
+      backButtonClicked: false
     });
   }
 
   creditsClick() {
     this.resetScrollPosition();
     this.setState({
-      page: 'CreditsPage'
+      page: 'CreditsPage',
+      backButtonClicked: false
     });
   }
 
@@ -115,36 +163,47 @@ export default class App extends React.Component {
     }
     else {
       const page = this.state.page;
+      const backButtonClicked = this.state.backButtonClicked;
       if (page === 'HomePage') {
-        window.history.pushState({}, "Viraj Chitnis", "/");
+        if (!backButtonClicked) {
+          window.history.pushState({}, "Viraj Chitnis", "/");
+        }
         document.title = 'Viraj Chitnis';
         dom = (
           <HomePage />
         );
       }
       else if (page === 'ResumePage') {
-        window.history.pushState({}, "Resumé - Viraj Chitnis", "/resume");
+        if (!backButtonClicked) {
+          window.history.pushState({}, "Resumé - Viraj Chitnis", "/resume");
+        }
         document.title = 'Resumé - Viraj Chitnis';
         dom = (
           <ResumePage />
         );
       }
       else if (page === 'AppsPage') {
-        window.history.pushState({}, "Apps - Viraj Chitnis", "/apps");
+        if (!backButtonClicked) {
+          window.history.pushState({}, "Apps - Viraj Chitnis", "/apps");
+        }
         document.title = 'Apps - Viraj Chitnis';
         dom = (
           <AppsPage />
         );
       }
       else if (page === 'WineryPage') {
-        window.history.pushState({}, "Winery - Viraj Chitnis", "/winery");
+        if (!backButtonClicked) {
+          window.history.pushState({}, "Winery - Viraj Chitnis", "/winery");
+        }
         document.title = 'Winery - Viraj Chitnis';
         dom = (
           <WineryPage />
         );
       }
       else if (page === 'CreditsPage') {
-        window.history.pushState({}, "Credits - Viraj Chitnis", "/credits");
+        if (!backButtonClicked) {
+          window.history.pushState({}, "Credits - Viraj Chitnis", "/credits");
+        }
         document.title = 'Credits - Viraj Chitnis';
         dom = (
           <CreditsPage />

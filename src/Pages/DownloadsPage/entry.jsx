7de10441c7cabca301a77d54156a62c561e9587c
Viraj Chitnis <mail@virajchitnis.com>
2026-07-01 13:29:11 -0400

    Remove decommissioned site ahead of full rewrite
    The old React/webpack/Express site has been retired for years. Full
    history is preserved at the v1-legacy tag and the legacy branch.

--- src/Pages/DownloadsPage/entry.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DownloadsPage from './DownloadsPage.jsx';
-import '../../App/scss/global.scss';
-
-ReactDOM.render(
-  <DownloadsPage />,
-  document.getElementById('root')
-);
